8407fdc75b8059ccca14e4f7946c75ac8a2304d2
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-10-27 17:09:12 -0300

    adding animation on scroll

--- scripts.js
+++ scripts.js
@@ -28,4 +28,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
             button.classList.toggle("toggled");
         }
     });
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+    
+            const targetId = this.getAttribute('href').substr(1);
+            const targetElement = document.getElementById(targetId);
+    
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
 });
